3bc420801b7acff63410f3a699f095784e8a71fe
github username <github email address>
2020-06-03 10:59:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# README
+
+## Project Description
+
+This Seattle Parks Application offers information on parks in the city of Seattle. This application will allow users to keep a record of all their trips, plan future trips and read about trips other people have taken. 
+
+This application hopes to inspire users to explore new parks in the city while staying safe during COVID-19.
+
+## Technologies Used
+This project uses Ruby and Rails with a postgres database on the back end, and JavaScript, React, CSS and HTML on the front end with JWT for authentication.
+* GeoCoder for geolocating
+* Bootstrap for CSS 
+* Chart.js for Graphs 
+* Google Maps
+* Weather API: https://www.metaweather.com/
+    * woeid=2490383 for Seattle, WA
+* Data from: http://data-seattlecitygis.opendata.arcgis.com/
+
+Things you may want to cover:
+
+* Ruby version
+* System dependencies
+* Configuration
+* Database creation
+    * rails db:create
+* Database initialization
+    * rails db:migrate
+    * rails db:seed
+* Deployment instructions
+
+## Models
+![alt text](/data/Models/jpg)
+
+## User Stories
+
+* Account
+    * User can login/ signup to the site (with authentication and validations)
+    * User can edit their account information
+
+* Parks
+    * User can see a list of Seattle Parks (With Maps)
+    * User can filter through parks by neighbouhood, area, features, already visited
+    * User can see a specific Park and its details 
+
+* Saved List
+    * User can save parks they want to visit 
+
+* Visits
+    * User can post a new visit to a park (with comments, rating, photo)
+    * User can plan a visit in the future
+    * User can see and edit all past visits
+    * User can delete upcoming visit
+
+* Weather
+    * User can see the upcoming weather 
+
+* Data 
+    * User can see the busiest parks (density)
+    * User can see data on visits vs weather 
+    * User can see what percentage of the parks they have visited
+
+## Color Pallete
+#2A3C2B
+#2F4550
+#6B9080
+#A4C3B2
+#F6FFF8
+#DB7C00
+
+## Trello 
+
+https://trello.com/b/BQ3YuPcK/mod-5-project
+
+## Future Updates
+* Transit Information to Each Park
+* Embed Instagram Hashtags for Each Park
+* Google Maps Direction
+* Extras: Admin Account, notifications, new user walkthrough, share on social media, sounds, file uploads
+
+## License
+This project is licensed under the GNU General Public License.
+
---
+++ db/seeds.rb
@@ -0,0 +1,117 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+array1=CSV.read("./data/Seattle_Parks.csv")
+##array of arrays #each inner array [id, name, area, length, shapearea]
+
+array2=CSV.read("./data/Seattle_Parks_and_Recreation_Parks_With_Features.csv")
+##array of arrays #each inner array [pmaid, name, feature_desc, hours, xpos, ypos, Location]
+
+array3=CSV.read("./data/Seattle_Parks_And_Recreation_Park_Addresses.csv")
+##array of arrays #each inner array [PMAID,LocID,Name,Address,ZIP Code,X Coord,Y Coord,Location 1]
+
+## FROM ARRAY 1
+
+all_parks=[]
+
+array1.each {|park| all_parks<<{name: park[1], area: park[5]}}
+all_parks.shift
+all_parks= all_parks.select{|park| park[:name] != nil}
+
+## FROM ARRAY 3
+all_parks2=[]
+parknames=[]
+
+array3.each do |park| 
+    all_parks2<<{name: park[2].upcase, address: park[3], x: park[5], y:park[6]} if !parknames.include?(park[2].upcase)
+    parknames<< park[2].upcase
+end
+
+all_parks2.shift
+
+all_parks.each do |park1|
+    all_parks2.each do |park2|
+        if park1[:name]==park2[:name]
+            park2[:area]=park1[:area]
+        end
+    end
+    # if !parknames.include?(park1[:name])
+    #     all_parks2<< park1
+    #     parknames<< park1[:name]
+    # end
+end
+
+# pp all_parks2
+pp all_parks2
+
+## FROM ARRAY 2
+
+features=["Golf",
+ "Play Area (ADA Compliant)",
+ "Basketball (Full)",
+ "Bike Trail",
+ "Play Area",
+ "Paths",
+ "Waterfront",
+ "P-Patch Community Garden",
+ "Soccer",
+ "View",
+ "Woods",
+ "Historic Landmark",
+ "Creek",
+ "Garden",
+ "Boat Launch (Hand Carry)",
+ "Adult Fitness Equipment",
+ "Guarded Beach",
+ "Restrooms (ADA Compliant)",
+ "Wading Pool or Water Feature",
+ "Tennis Lights",
+ "Picnic Sites",
+ "Disc Golf",
+ "Restrooms",
+ "Weddings and Ceremonies",
+ "Green Space",
+ "Paths (ADA Compliant)",
+ "Baseball/Softball",
+ "Fishing",
+ "Basketball (Half)",
+ "Rental Facility",
+ "Hiking Trails",
+ "Pesticide Free",
+ "Decorative Fountain",
+ "Tennis Court (Outdoor)",
+ "Horseshoe Pits",
+ "Dog Off Leash Area",
+ "Skatepark",
+ "Football",
+ "Marination Ma Kai",
+ "Lawn Bowling",
+ "Tennis Backboard (Outdoor)",
+ "Scuba Diving",
+ "Community Building",
+ "Environmental Learning Center",
+ "NO Beach Access",
+ "Fire Pit",
+ "Pool (Outdoor)",
+ "Lacrosse",
+ "Boat Moorage",
+ "Picnic Sites (ADA Compliant)",
+ "Skatespot",
+ "Model Boat Pond",
+ "Community Center",
+ "Cricket",
+ "Ultimate",
+ "Track",
+ "Rugby",
+ "Boat Launch (Motorized)",
+ "Bike Polo",
+ "Pool (Indoor)",
+ "T-Ball",
+ "Pickleball Court",
+ "Flag Football"]
