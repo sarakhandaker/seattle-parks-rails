b70b7b2e6e2a0c194efb352f4509f42690d918aa
github username <github email address>
2020-06-09 15:51:28 -0700

    added secrets

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins Rails.application.secrets.allowed_origins
 
     resource '*',
       headers: :any,
