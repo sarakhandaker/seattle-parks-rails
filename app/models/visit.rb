13007da33b76f4d0efc228c58227a9efdb3af7bd
github username <github email address>
2020-06-11 10:53:11 -0700

    date validation

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -6,4 +6,9 @@ class Visit < ApplicationRecord
     validates :rating, length: { in: 0..5 }
     validates :rating, presence: true
     validates :date, presence: true
+
+    validates :user, :uniqueness => {:scope => [:date, :park], 
+    message: ->(object, data) do
+        "the visit to this park on this date has already been saved."
+      end}
 end
