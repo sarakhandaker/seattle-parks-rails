862e34a5c3de379a0ca28ad6a4e36b2f3f7b1af2
github username <github email address>
2020-06-16 10:26:00 -0700

    origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  ENV['CORS_ORIGINS']
+    origins  [ENV['CORS_ORIGINS_1'], ENV['CORS_ORIGINS_2']]
  
     resource '*',
       headers: :any,
