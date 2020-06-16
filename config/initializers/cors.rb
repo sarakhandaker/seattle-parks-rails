af497cdffe4bf46c2d6fa3ce53bc8da92e2582db
github username <github email address>
2020-06-16 10:22:56 -0700

    origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3001','https://cpark-app.herokuapp.com']
+    origins  ENV['CORS_ORIGINS']
  
     resource '*',
       headers: :any,
